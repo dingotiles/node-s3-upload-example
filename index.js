00808ce38963ef3f987dcf634e7c48b0c43b64a3
Justin Carter <jcarter@starkandwayne.com>
2016-01-27 13:20:18 +0100

    Simple image uploader

--- index.js
+++ index.js
@@ -5,36 +5,82 @@ var aws = require('aws-sdk');
 var appEnv = cfenv.getAppEnv()
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+
+/* Configure AWS sdk
+ */
 var s3creds = appEnv.getServiceCreds('node-s3-example-s3')
+aws.config.update({
+  accessKeyId: s3creds.access_key_id,
+  secretAccessKey: s3creds.secret_access_key,
+  signatureVersion: 'v4'
+});
 
-var AWS_ACCESS_KEY = s3creds.access_key_id;
-var AWS_SECRET_KEY = s3creds.secret_access_key;
-var S3_BUCKET = s3creds.bucket
+var BUCKET_NAME = s3creds.bucket;
 
-aws.config.update({accessKeyId: AWS_ACCESS_KEY , secretAccessKey: AWS_SECRET_KEY });
-aws.config.update({region: '' , signatureVersion: 'v4' });
-
-app.get('/', function (req, res) {
-  var s3 = new aws.S3();
-  var path = 'node/hello_world'
-
+/* Set CORS Configuration for Bucket
+ * to allow access from this apps URLs
+ */
+(function() {
   var params = {
-    Bucket: S3_BUCKET,
-    Key: path,
-    Body: 'Hello! This is the content of the file ' + path + ' in bucket: ' + S3_BUCKET
+    Bucket: BUCKET_NAME,
+    CORSConfiguration: {
+      CORSRules: [
+        {
+          AllowedMethods: [ 'GET', 'POST', 'PUT' ],
+          AllowedOrigins: appEnv.urls,
+          AllowedHeaders: [ '*' ]
+        },
+      ]
+    }
   };
-
-  s3.putObject(params, function(err, data) {
-
-    if (err)
-      console.log(err)
-    else
-      console.log("Successfully uploaded data to " + path);
-
+  var s3 = new aws.S3();
+  s3.putBucketCors(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
   });
-  res.send('A File has been successfully uploaded');
-})
+})();
 
+/* Respond to GET requests to /.
+ * Upon request, render the 'index.html' web page in views/ directory.
+ */
+app.get('/', function(req, res){
+  res.render('index.html');
+});
+
+/* Respond to GET requests to /sign_s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and the
+ * anticipated URL of the image.
+ */
+app.get('/sign_s3', function(req, res){
+  var s3 = new aws.S3();
+  var s3_params = {
+    Bucket: BUCKET_NAME,
+    Key: req.query.file_name,
+    Expires: 60,
+    ContentType: req.query.file_type,
+    ACL: 'public-read'
+  };
+  s3.getSignedUrl('putObject', s3_params, function(err, data){
+    if(err){
+      console.log(err);
+    }
+    else{
+      var return_data = {
+        signed_request: data,
+        url: 'https://'+BUCKET_NAME+'.s3.amazonaws.com/'+req.query.file_name
+      };
+      res.write(JSON.stringify(return_data));
+      res.end();
+    }
+  });
+});
+
+/*
+ * Start the server to handle incoming requests.
+ */
 app.listen(appEnv.port, appEnv.bind, function() {
   console.log("server starting on " + appEnv.url)
 })

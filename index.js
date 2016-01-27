2c977ef3dc5ee6fef9e5e31ddad8fecdb7226f2b
Justin Carter <jcarter@starkandwayne.com>
2016-01-27 09:53:07 +0100

    Output creds in /

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var cfenv = require("cfenv")
 var appEnv = cfenv.getAppEnv()
 var app = express();
 
+var s3creds = appEnv.getServiceCreds('node-s3-example-s3')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!\n' + JSON.stringify(s3creds));
 });
 
 app.listen(appEnv.port, appEnv.bind, function() {
